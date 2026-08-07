b4ed06dab30b8ae9db50708e60114e652a9ebb81
Mariana Oliveira <marii2004.maa66@gmail.com>
2026-08-07 17:54:51 -0300

    feat: adiciona interfaces do repository

---
+++ src/main/java/com/neki/skills/repository/UsuarioRepository.java
@@ -0,0 +1,12 @@
+package com.neki.skills.repository;
+
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.neki.skills.model.Usuario;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
+    Optional<Usuario> findByLogin(String login);
+}
